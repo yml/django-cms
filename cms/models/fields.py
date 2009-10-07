062275b1161f5d3350ce3e63f86974d7ce0650e4
User <cms2@Mainframe2.levangernett.no>
2009-10-07 21:41:49 +0200

    set or create works on manager and descriptor

--- cms/models/fields.py
+++ cms/models/fields.py
@@ -1,27 +1,30 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
+from cms import settings
 from cms.utils.i18n import get_fallback_languages
 
 class TranslationDescriptor(object):
 
-    def __init__(self, model, translationmodel_field, reverse_name):
+    def __init__(self, name, model, translationmodel_field, reverse_name, translation_cache_name, languages_cache_name):
+        self.name = name
         self.model = model
         self.translationmodel_field = translationmodel_field
         self.reverse_name = reverse_name
-        self.translation_cache_name = self.model.__name__.lower() + '_translation_cache'
-        self.languages_cache_name = self.model.__name__.lower() + '_language_cache'
+        self.translation_cache_name = translation_cache_name
+        self.languages_cache_name = languages_cache_name
 
     def __get__(self, instance=None, owner=None):
         if instance is None:
             raise AttributeError(
                 "The '%s' attribute can only be accessed from %s instances."
                 % (self.name, owner.__name__))
+        instance.__dict__[self.name] = self
         self.instance = instance
+        return self
  
     def __set__(self, instance, value):
-        raise AttributeError(
-            "The '%s' attribute can not be set" % self.name)
-        
+        instance.__dict__[self.name] = self
+            
     def get_languages(self):
         """
         get the list of all existing languages for this page
@@ -42,8 +45,8 @@ class TranslationDescriptor(object):
         """
         
         language = self._get_translation_cache(language)
-        if language in self.translation_cache:
-            return self.translation_cache[language]
+        if language in self.instance.__dict__[self.translation_cache_name]:
+            return self.instance.__dict__[self.translation_cache_name] 
         from cms.models.titlemodels import EmptyTitle
         return EmptyTitle()
     
@@ -56,11 +59,12 @@ class TranslationDescriptor(object):
             return None
 
     def __getattr__(self, name):
-        if name.find('_') and name.split('_', -1)[1] in [l[0] in settings.LANGUAGES]:
+        if name.find('_') and name.split('_', -1)[-1] in [l[0] for l in settings.LANGUAGES]:
             lang = name.split('_', -1)[1]
             attr = name.split('_', -1)[0] 
             return self.get_translation_obj_attribute(self, attr, lang)
- 
+        raise AttributeError
+
     def _get_translation_cache(self, language=None):
         if not language:
             language = get_language()
@@ -98,11 +102,19 @@ class TranslationDescriptor(object):
             obj.pk = None
             setattr(obj, self.translationmodel_field, object)
             obj.save()
+
+    def set_or_create(self, language=None, **kwargs):
+        return self.model.objects.set_or_create(self.instance, language=language, **kwargs)        
+        
      
 class TranslationsManager(models.Manager): # um, just use the regular manager and override if publisher is needed?
 
-    def __init__(self, translationmodel_field):
+    def __init__(self, translationmodel_field, translation_cache_name, languages_cache_name):
         self.translationmodel_field = translationmodel_field
+
+        self.translation_cache_name = translation_cache_name
+        self.languages_cache_name = languages_cache_name
+
         super(TranslationsManager, self).__init__()
 
     def get_translation(self, for_object, language, language_fallback=False):
@@ -129,16 +141,17 @@ class TranslationsManager(models.Manager): # um, just use the regular manager an
                 raise
         return None
     
-    def get_slug(self, slug, site=None, use_site=False):
+    def get_slug(self, slug, site=None, site_field=False):
         """
         Returns the latest slug for the given slug and checks if it's available
         on the current site.
         """
         kw = {}
-        if use_site:
+        if site_field:
             if not site:
                 site = Site.objects.get_current()
-            kw[self.translationmodel_field + '__site'] = site
+            kw[self.translationmodel_field + '__' + site_field] = site
+
         kw['slug'] = slug
 
         try:
@@ -148,16 +161,15 @@ class TranslationsManager(models.Manager): # um, just use the regular manager an
         else:
             return translations 
         
-    def set_or_create(self, for_object, language, **kwargs):
+    def set_or_create(self, for_object, language=None, **kwargs):
         """
         set or create a title for a particular page and language
         """
         try:
             obj = self.get(language=language, **{self.translationmodel_field: for_object})
-            for key, value in kwargs:
+            for key, value in kwargs.items():
                 if not value is None:
                     setattr(obj, key, value)
-
         except self.model.DoesNotExist:
             kwargs[self.translationmodel_field] = for_object
             obj = self.model(language=language, **kwargs)
@@ -168,25 +180,35 @@ class TranslationsManager(models.Manager): # um, just use the regular manager an
         return obj
 
     def set_custom(self, obj, **kwargs):
+        """
         if 'overwrite_url' in kwargs:
             obj.has_url_overwrite = True
             obj.path = kwargs['overwrite_url']
         else:
             obj.has_url_overwrite = False
+        """
 
 class TranslationForeignKey(models.ForeignKey):
 
     def __init__(self, to, **kwargs):
-        self.translations_attribute = kwargs.pop('translation_attribute')
-        self.translations_manager = kwargs.pop('translations_manager', TranslationsManager)
+
+        self.translation_attribute = kwargs.pop('translation_attribute')
+        self.translation_manager = kwargs.pop('translation_manager', TranslationsManager)
+        self.translation_descriptor = kwargs.pop('translation_descriptor', TranslationDescriptor)
+
         super(TranslationForeignKey, self).__init__(to, **kwargs)
+        
+        self.translation_cache_name = self.rel.to.__name__.lower() + '_translation_cache'
+        self.languages_cache_name = self.rel.to.__name__.lower() + '_language_cache'
 
     def contribute_to_class(self, cls, name):
         super(TranslationForeignKey, self).contribute_to_class(cls, name)
-        cls.add_to_class('objects', self.translations_manager(self.name))
+        cls.add_to_class('objects', self.translation_manager(self.name, self.translation_cache_name, self.languages_cache_name))
 
     def contribute_to_related_class(self, cls, related):
         super(TranslationForeignKey, self).contribute_to_related_class(cls, related)
-        setattr(cls, self.translations_attribute, 
-            TranslationDescriptor(related.model, self.name, related.get_accessor_name())
+        setattr(cls, self.translation_attribute,
+            self.translation_descriptor(
+                self.translation_attribute, related.model, self.name, related.get_accessor_name(),
+                self.translation_cache_name, self.languages_cache_name)
         )
