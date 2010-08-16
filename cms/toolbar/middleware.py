2b178d59dddfe1b248861f090db1c565a38bf18a
Yann Malet <yann.malet@gmail.com>
2010-08-17 00:48:59 +0200

    Move the show_toolbar method and the get_toolbar_context into the Middleware class.

--- cms/toolbar/middleware.py
+++ cms/toolbar/middleware.py
@@ -16,77 +16,74 @@ from django.utils import simplejson
 from django.utils.encoding import smart_unicode
 from django.forms.widgets import Media
 
-def show_toolbar(request):
-    if request.is_ajax():
-        return False
-    try:
-        if request.path_info.startswith(reverse("admin:index")):
-            return False
-    except NoReverseMatch:
-        pass
-    if request.path_info.startswith(urlparse.urlparse(settings.MEDIA_URL)[2]):
-        return False
-    if "edit" in request.GET:
-        return True
-    if not hasattr(request, "user"):
-        return False
-    if not request.user.is_authenticated() or not request.user.is_staff:
-        return False
-    return True
 
-def get_toolbar_context(request):
-    from cms.plugin_pool import plugin_pool
-    from cms.utils.admin import get_admin_menu_item_context
-    """
-    Renders the Toolbar.
-    """
-    has_auth = request.user.is_authenticated() and request.user.is_staff
-    edit = request.session.get('cms_edit', False) and has_auth
-    page = request.current_page
-    move_dict = []
-    if edit and page:
-        template = get_template_from_request(request)
-        placeholders = get_placeholders(template)
-        for placeholder in placeholders:
-            d = {}
-            name = cms_settings.CMS_PLACEHOLDER_CONF.get("%s %s" % (page.get_template(), placeholder), {}).get("name", None)
-            if not name:
-                name = cms_settings.CMS_PLACEHOLDER_CONF.get(placeholder, {}).get("name", None)
-            if not name:
-                name = placeholder
-            d['name'] = title(name)
-            plugins = plugin_pool.get_all_plugins(placeholder, page)
-            d['plugins'] = [] 
-            for p in plugins:
-                d['plugins'].append(p.value)
-            d['type'] = placeholder
-            move_dict.append(d)
-        data = safe(simplejson.dumps(move_dict))
-    else:
-        data = {}
-    if has_auth and page:
-        context = get_admin_menu_item_context(request, page, filtered=False)
-    else:
-        context = {}
-    context.update({
-        'has_auth':has_auth,
-        'page':page,
-        'templates': cms_settings.CMS_TEMPLATES,
-        'auth_error':not has_auth and 'cms_username' in request.POST,
-        'placeholder_data':data,
-        'edit':edit,
-        'CMS_MEDIA_URL': cms_settings.CMS_MEDIA_URL,
-    })
-    return context
 
 class ToolbarMiddleware(object):
     """
     Middleware to set up CMS Toolbar.
     """
-    def __init__(self, show_toolbar_func=show_toolbar,
-                 get_toolbar_context_func=get_toolbar_context):
-        self.show_toolbar = show_toolbar_func
-        self.get_toolbar_context = get_toolbar_context_func
+    def show_toolbar(self, request):
+        if request.is_ajax():
+            return False
+        try:
+            if request.path_info.startswith(reverse("admin:index")):
+                return False
+        except NoReverseMatch:
+            pass
+        if request.path_info.startswith(urlparse.urlparse(settings.MEDIA_URL)[2]):
+            return False
+        if "edit" in request.GET:
+            return True
+        if not hasattr(request, "user"):
+            return False
+        if not request.user.is_authenticated() or not request.user.is_staff:
+            return False
+        return True
+    
+    def get_toolbar_context(self, request):
+        from cms.plugin_pool import plugin_pool
+        from cms.utils.admin import get_admin_menu_item_context
+        """
+        Renders the Toolbar.
+        """
+        has_auth = request.user.is_authenticated() and request.user.is_staff
+        edit = request.session.get('cms_edit', False) and has_auth
+        page = request.current_page
+        move_dict = []
+        if edit and page:
+            template = get_template_from_request(request)
+            placeholders = get_placeholders(template)
+            for placeholder in placeholders:
+                d = {}
+                name = cms_settings.CMS_PLACEHOLDER_CONF.get("%s %s" % (page.get_template(), placeholder), {}).get("name", None)
+                if not name:
+                    name = cms_settings.CMS_PLACEHOLDER_CONF.get(placeholder, {}).get("name", None)
+                if not name:
+                    name = placeholder
+                d['name'] = title(name)
+                plugins = plugin_pool.get_all_plugins(placeholder, page)
+                d['plugins'] = [] 
+                for p in plugins:
+                    d['plugins'].append(p.value)
+                d['type'] = placeholder
+                move_dict.append(d)
+            data = safe(simplejson.dumps(move_dict))
+        else:
+            data = {}
+        if has_auth and page:
+            context = get_admin_menu_item_context(request, page, filtered=False)
+        else:
+            context = {}
+        context.update({
+            'has_auth':has_auth,
+            'page':page,
+            'templates': cms_settings.CMS_TEMPLATES,
+            'auth_error':not has_auth and 'cms_username' in request.POST,
+            'placeholder_data':data,
+            'edit':edit,
+            'CMS_MEDIA_URL': cms_settings.CMS_MEDIA_URL,
+        })
+        return context
 
     def process_request(self, request):
         if request.method == "POST":
